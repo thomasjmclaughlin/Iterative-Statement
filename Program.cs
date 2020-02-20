c8aa7f2ed38619d204b7f4d549e269837cbd7d92
Thomas <tjmclaughlin@mail.usf.edu>
2020-02-19 20:19:51 -0500

    Added check to see if input is even or odd

--- Program.cs
+++ Program.cs
@@ -8,9 +8,25 @@ namespace Iterative_Statement
         {
             
             // Empty variable that will be replaced with string of even or odd
-            inputDetermination = "";
+            string inputDetermination = "";
 
+            //Asks user to input integer, saves it as userInt
             Console.WriteLine("Please enter an integer value between 2 and 10");
+            int userInt = Int32.Parse(Console.ReadLine());
+
+            //check if input is even or odd
+            if (userInt % 2 == 1)
+            {
+                //set inputDetermination to odd
+                inputDetermination = "odd";
+            }
+            else
+            {
+                //set inputDetermination to even
+                inputDetermination = "even";
+            }
+
+            Console.WriteLine("Your integer is " + inputDetermination);
         }
     }
 }
