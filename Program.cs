c40211b492b13dbbe0a85f35ce9faa5a6a757f56
Thomas <tjmclaughlin@mail.usf.edu>
2020-02-19 20:07:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Iterative_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer value between 2 and 10");
+        }
+    }
+}
